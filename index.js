f717df8762c6195d6c43f39fd35a30db386d2c96
James Plasencia <jamesplasencia@gmail.com>
2022-07-09 17:43:55 -0700

    added a cards.js file to help create cards  finished up the inquiry qs

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
-const Inter = require('./lib/Intern')
+const Intern = require('./lib/Intern')
+const teamCards = require('./src/cards')
+const path = require('path')
+const assembleTeam = [];
 console.log("we out here")
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 const promptManager  = () =>{
 return inquirer.prompt(
  [
-    // list input for type of job-have something that changes the last questions based on it?
-    
-    // when inquirer statement-run specific question-extra question at the end
     {
         type: 'input',
         name: 'name',
@@ -29,9 +26,6 @@ return inquirer.prompt(
         name: 'email',
         message: 'What is your email?',
     },
-    //more info-maybe and if-then for manager/engineer/intern? when statement-built into the question object
-    {
-    },
     {
         type: 'input',
         name: 'officeNumber',
@@ -46,6 +40,7 @@ assembleTeam.push(manager);
 anothaOne();
 })
 };
+// to see if we want to add another employee or create the team
 const anothaOne = () =>{
     return inquirer.prompt([
         {
@@ -57,55 +52,95 @@ const anothaOne = () =>{
     ])
     .then(choice =>{
 switch (choice.job){
-    case "add an engineer":
+    case "Engineer":
     promptEngineer();
     break;
-    case "add an intern":
+    case "Intern":
     promptIntern();
     break;
     default:
-    assembleTeam();
+    teamAssembly();
 }
     })
+};
+// engineer questions
+const promptEngineer  = () =>{
+    return inquirer.prompt(
+     [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Do you have and ID number.',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What if your Github account?',
+         },
+        ]
+    )
+    .then(answers =>{
+    console.log (answers);
+    const engineer = new Engineer (answers.name, answers.id, answers.email, answers.github)
+    assembleTeam.push(engineer);
+    anothaOne();
+    })
+    };
+
+    // Intern questions
+    const promptIntern  = () =>{
+        return inquirer.prompt(
+         [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Do you have and ID number.',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school are you attending?',
+            },
+            ]
+        )
+        .then(answers =>{
+        console.log (answers);
+        const intern = new Intern (answers.name, answers.id, answers.email, answers.school)
+        assembleTeam.push(intern);
+        anothaOne();
+        })
+        };
+const teamAssembly = () => {
+    console.log("DEV TEAM, ASSEEEEBLE!")
+    fs.writeFileSync(outputPath, teamCards(assembleTeam))
 }
-//     type: 'input',
-//     name: 'engineer',
-//     message: 'What if your Github account?',
-// },
-// {
-    //     type: 'input',
-    //     name: 'intern',
-    //     message: 'What school are you attending?',
-    
-
-// function writeToFile(filename, data) {
-
-//     fs.writeFile(filename, data, (error) => { if (error) throw error; console.log("success!") });
-// }
-
-// for loop for the returned data. how to combine data from manage, engineer, intern all in one loop?
-
-
-
-// second function to add another employee or build the team-either calls function or hand off info (write to file)
+promptManager();
 
 // WHEN I click on an email address in the HTML
 // THEN my default email program opens and populates the TO field of the email with the address
 
 // WHEN I click on the GitHub username
 // THEN that GitHub profile opens in a new tab
-
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
---
+++ src/cards.js
@@ -0,0 +1,16 @@
+const manCards = manager => {
+return `<div class="card" style="width: 18rem;">
+<div class="card-body">
+<h5 class="card-subtitle mb-2">${answers.name}</h5>
+  <h5 class="card-title"Manager</h5>
+  <ul class="list-group list-group-flush">
+  <li class="list-group-item">ID ${answers.id}</li>
+  <li class="list-group-item">Email ${answers.email}</li>
+  <li class="list-group-item">Office Number ${answers.officeNumber}</li>
+</ul>
+  <a href="#" class="card-link">Card link</a>
+
+</div>
+</div>`
+
+}
